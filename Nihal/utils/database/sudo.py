15cdf961abe1318d2e9ffcf424214fe9055370f2
MrNihalX <109473632+MrNihalX@users.noreply.github.com>
2022-10-04 21:06:53 +0530

    Create sudo.py

---
+++ Nihal/utils/database/sudo.py
@@ -0,0 +1,36 @@
+# A Powerful Music Bot Property Of Redox Indian Largest Chatting Group
+# Without Credit (Mother Fucker)
+# Redox © @Mr_Nihal9 © REDOXMOD
+# Mr Nihal
+
+
+from typing import Dict, List, Union
+
+from Nobita.core.mongo import mongodb
+
+sudoersdb = db.sudoers
+
+
+async def get_sudoers() -> list:
+    sudoers = await sudoersdb.find_one({"sudo": "sudo"})
+    if not sudoers:
+        return []
+    return sudoers["sudoers"]
+
+
+async def add_sudo(user_id: int) -> bool:
+    sudoers = await get_sudoers()
+    sudoers.append(user_id)
+    await sudoersdb.update_one(
+        {"sudo": "sudo"}, {"$set": {"sudoers": sudoers}}, upsert=True
+    )
+    return True
+
+
+async def remove_sudo(user_id: int) -> bool:
+    sudoers = await get_sudoers()
+    sudoers.remove(user_id)
+    await sudoersdb.update_one(
+        {"sudo": "sudo"}, {"$set": {"sudoers": sudoers}}, upsert=True
+    )
+    return True
