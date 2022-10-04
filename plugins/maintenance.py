8e6239fefdbbd4b3828e8513f502e8cfa680f17b
MrNihalX <109473632+MrNihalX@users.noreply.github.com>
2022-10-05 03:43:02 +0530

    Create maintenance.py

---
+++ plugins/maintenance.py
@@ -0,0 +1,46 @@
+from pyrogram import filters
+from pyrogram.types import Message
+
+from Nihal.strings import get_command, get_string
+from Nihal import app
+from Nihal.misc import SUDOERS
+from Nihal.utils.database import (get_lang, is_maintenance,
+                                       maintenance_off,
+                                       maintenance_on)
+from Nihal.utils.decorators.language import language
+
+# Commands
+MAINTENANCE_COMMAND = get_command("MAINTENANCE_COMMAND")
+
+
+@app.on_message(filters.command(MAINTENANCE_COMMAND) & SUDOERS)
+async def maintenance(client, message: Message):
+    try:
+        language = await get_lang(message.chat.id)
+        _ = get_string(language)
+    except:
+        _ = get_string("en")
+    usage = _["maint_1"]
+    if len(message.command) != 2:
+        return await message.reply_text(usage)
+    message.chat.id
+    state = message.text.split(None, 1)[1].strip()
+    state = state.lower()
+    if state == "enable":
+        if await is_maintenance() is False:
+            await message.reply_text(
+                "📍 ᴍᴀɪɴᴛᴇɴᴀɴᴄᴇ ᴍᴏᴅᴇ ɪs ᴀʟʀᴇᴀᴅʏ ᴇɴᴀʙʟᴇᴅ ✅."
+            )
+        else:
+            await maintenance_on()
+            await message.reply_text(_["maint_2"])
+    elif state == "disable":
+        if await is_maintenance() is False:
+            await maintenance_off()
+            await message.reply_text(_["maint_3"])
+        else:
+            await message.reply_text(
+                "🌷 ᴍᴀɪɴᴛᴇɴᴀɴᴄᴇ ᴍᴏᴅᴇ ɪs ᴅɪsᴀʙʟᴇᴅ ❌."
+            )
+    else:
+        await message.reply_text(usage)
