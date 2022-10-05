b62911d58005bdb04d0174bc0e7252625df6fcde
MrNihalX <109473632+MrNihalX@users.noreply.github.com>
2022-10-05 11:36:47 +0530

    Create webshot.py

---
+++ plugins/webshot.py
@@ -0,0 +1,32 @@
+# A Powerful Music Bot Property Of Redox Indian Largest Chatting Group
+# Without Credit (Mother Fucker)
+# Redox © @Mr_Nihal9 © REDOXMOD
+# Mr Nihal
+from pyrogram import filters
+from pyrogram.types import Message
+
+from Nobita import app
+from Nobita.utils.errors import capture_err
+
+
+@app.on_message(filters.command("webss"))
+@capture_err
+async def take_ss(_, message: Message):
+    try:
+        if len(message.command) != 2:
+            return await message.reply_text(
+                "**» ɢɪᴠᴇ ᴀ ᴜʀʟ ᴛᴏ ғᴇᴛᴄʜ sᴄʀᴇᴇɴsʜᴏᴛ...**"
+            )
+        url = message.text.split(None, 1)[1]
+        m = await message.reply_text("**» ᴛʀʏɪɴɢ ᴛᴏ ᴛᴀᴋᴇ sᴄʀᴇᴇɴsʜᴏᴛ...**")
+        await m.edit("**» ᴜᴩʟᴏᴀᴅɪɴɢ ᴄᴀᴩᴛᴜʀᴇᴅ sᴄʀᴇᴇɴsʜᴏᴛ...**")
+        try:
+            await message.reply_photo(
+                photo=f"https://webshot.amanoteam.com/print?q={url}",
+                quote=False,
+            )
+        except TypeError:
+            return await m.edit("**» ɴᴏ sᴜᴄʜ ᴡᴇʙsɪᴛᴇ.**")
+        await m.delete()
+    except Exception as e:
+        await message.reply_text(str(e))
